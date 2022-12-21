2669fcc7d65934e349293824b7f13a042fc29647
RobertoSanchez <roberto.sanchez@formos.com>
2022-12-21 08:41:43 -0500

    Web: Create common code

--- src/main/groovy/com/zoominfo/catcherweb/CatcherWebApplication.groovy
+++ src/main/groovy/com/zoominfo/catcherweb/CatcherWebApplication.groovy
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 @SpringBootApplication
 class CatcherWebApplication {
 
-	static void main(String[] args) {
-		SpringApplication.run(CatcherWebApplication, args)
-	}
+    static void main(String[] args) {
+        SpringApplication.run(CatcherWebApplication, args)
+    }
 
 }
--- src/test/groovy/com/zoominfo/catcherweb/CatcherWebApplicationTests.groovy
+++ src/test/groovy/com/zoominfo/catcherweb/CatcherWebApplicationTests.groovy
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest
 @SpringBootTest
 class CatcherWebApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
